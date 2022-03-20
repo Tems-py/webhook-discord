bde47c3f67f71b2cfa9e1506d25a3ae0d565056b
Tems <79542785+Tems-py@users.noreply.github.com>
2022-03-20 15:11:35 +0100

    new

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-import PySimpleGUI as sg
-from discord_webhook import DiscordWebhook
+import tkinter as ttk
+import requests
+import threading
 
-layout = [  [sg.Text("Insert webhook url")],
-            [sg.Input()],
-            [sg.Button('Ok'), sg.Button('Quit')]]
 
-window = sg.Window('Discord Webhook Tool by Tems', layout)     
+class Window(ttk.Frame):
+    def __init__(self, master=None):
+        ttk.Frame.__init__(self, master)
+        self.master = master
 
-event, url = window.read()
-window.close()
-if event == 'Quit':
-    window.close()
-    quit()
+
+# initialize tkinter
+root = ttk.Tk()
+app = Window(root)
+root.geometry('400x200')
+
+
+def ee():
+    data = {
+        'content': content.get()
+    }
+    if username.get(): data["username"] = username.get()
+    if avatar.get(): data["avatar_url"] = username.get()
+    x = requests.post(webhook_entry.get(), data=data)
+    print(x)
+
 
 def send():
-    second = [  [sg.Text("Message")],     
-            [sg.Input()],
-            [sg.Button('Ok'), sg.Button('Quit')]]
+    threading.Thread(target=ee).start()
 
 
-    tt = sg.Window('Discord Webhook Tool by Tems', second)
+ttk.Label(root, text="Webhook URL*", font=('Arial', 14)).grid(row=1, column=1)
+webhook_entry = ttk.Entry(root, bd=1, show=None, font=('Arial', 14))
+webhook_entry.grid(row=1, column=2)
 
-    event, mess = tt.read()
-    if event == 'Quit':
-        tt.close()
-        quit()
-    print(mess)
-    webhook = DiscordWebhook(url=url[0], content = mess[0] )
-    response = webhook.execute()
-    tt.close()
-while True:
-    send()
+ttk.Label(root, text="Content*", font=('Arial', 14)).grid(row=2, column=1)
+content = ttk.Entry(root, bd=1, show=None, font=('Arial', 14))
+content.grid(row=2, column=2)
+
+ttk.Label(root, text="Username", font=('Arial', 14)).grid(row=3, column=1)
+username = ttk.Entry(root, bd=1, show=None, font=('Arial', 14))
+username.grid(row=3, column=2)
+
+ttk.Label(root, text="Avatar url", font=('Arial', 14)).grid(row=4, column=1)
+avatar = ttk.Entry(root, bd=1, show=None, font=('Arial', 14))
+avatar.grid(row=4, column=2)
+
+M3 = ttk.Button(root, text="Send", command=send)
+M3.grid(row=7, column=2)
+
+# set window title
+root.wm_title("Discord webhook")
+root.iconbitmap('favicon.ico')
+
+# show window
+root.mainloop()
